14b21ce70911d539cf3596acb6861323e130f337
Carson Bring <carsonbring@gmail.com>
2025-01-27 22:21:02 -0600

    Rewriting HMM class to be more extensible and usable for Baum-Welch

--- hmm.py
+++ hmm.py
@@ -2,43 +2,67 @@ import torch
 import torch.nn as nn
 import torch.nn.functional as func
 
-# Going to need a day just to wrap my head around Baum-Welch because it is very complicated
-
 
 # Gradient optimization
-def gradient_train_hmm(hmm, sequences, num_epochs=50, lr=1e-2):
-    """
-    sequences: a list or tensor of observation sequences.
-    each sequence is shape (T,) with T being the number of time steps
-    Each sequence has integer observation indices
-    """
-    optimizer = torch.optim.Adam(hmm.parameters(), lr=lr)
-
-    for epoch in range(num_epochs):
-        optimizer.zero_grad()
-        total_log_prob = 0.0
-        for seq in sequences:
-            total_log_prob += hmm.log_likelihood(seq)
-
-        loss = -total_log_prob  # negative log-likelihood
-        loss.backward()  # pyright:ignore
-        optimizer.step()
-
-        print(f"Epoch {epoch}: NLL={loss.item():.3f}")  # pyright:ignore
-
-
-class DiscreteHMM(nn.Module):
+# def gradient_train_hmm(hmm, sequences, num_epochs=50, lr=1e-2):
+#     """
+#     sequences: a list or tensor of observation sequences.
+#     each sequence is shape (T,) with T being the number of time steps
+#     Each sequence has integer observation indices
+#     """
+#     optimizer = torch.optim.Adam(hmm.parameters(), lr=lr)
+#
+#     for epoch in range(num_epochs):
+#         optimizer.zero_grad()
+#         total_log_prob = 0.0
+#         for seq in sequences:
+#             total_log_prob += hmm.log_likelihood(seq)
+#
+#         loss = -total_log_prob  # negative log-likelihood
+#         loss.backward()  # pyright:ignore
+#         optimizer.step()
+#
+#         print(f"Epoch {epoch}: NLL={loss.item():.3f}")  # pyright:ignore
+#
+#
+class HMM(nn.Module):
     def __init__(self, num_states, num_observations):
-        super().__init__()
+        super(HMM, self).__init__()
         self.num_states = num_states
         self.num_observations = num_observations
 
         # Raw (unconstrained) parameters:
-        self.log_pi = nn.Parameter(torch.zeros(num_states))  # shape: (M,)
-        self.log_A = nn.Parameter(torch.zeros(num_states, num_states))  # shape: (M, M)
-        self.log_B = nn.Parameter(
-            torch.zeros(num_states, num_observations)
-        )  # shape: (M, V)
+        # unnormalized state priors
+        self.pi = nn.Parameter(torch.zeros(num_states))
+        self.transition_model = nn.Parameter(torch.zeros(num_states, num_states))
+
+        self.emission_model = nn.Parameter(torch.zeros(num_states, num_observations))
+
+    def sample(self, T=10):
+        state_priors = nn.functional.softmax(self.pi, dim=0)
+        transition_matrix = torch.nn.functional.softmax(self.transition_model, dim=0)
+        emission_matrix = torch.nn.functional.softmax(self.emission_model, dim=1)
+        z_t = torch.distributions.categorical.Categorical(state_priors).sample().item()
+        z = []
+        x = []
+        z.append(z_t)
+        for t in range(0, T):
+            x_t = (
+                torch.distributions.categorical.Categorical(emission_matrix[z_t])  # pyright: ignore
+                .sample()
+                .item()
+            )
+            x.append(x_t)
+
+            z_t = (
+                torch.distributions.categorical.Categorical(transition_matrix[z_t])  # pyright: ignore
+                .sample()
+                .item()
+            )
+            if t < T - 1:
+                z.append(x_t)
+
+        return x, z
 
     def forward(self, observations):
         return self.log_likelihood(observations)
@@ -58,30 +82,33 @@ class DiscreteHMM(nn.Module):
                 [self._forward_log_observations(seq) for seq in observations]
             ).sum()  # or .mean()
 
-    def _forward_log_observations(self, obs_seq):
+    def _forward_log_observations(self, x, T):
         """
-        obs_seq: shape (T,) with integer observation symbols in [0, num_observations-1].
-        Returns log P(obs_seq).
+        x : IntTensor of shape (batch size, T_max)
+        T : IntTensor of shape (batch_size,)
+
+        Returns log p(x).
+        T = length of each example
         """
-        # T = length of the sequence
-        T = obs_seq.shape[0]
+        if self.is_cuda:
+            x = x.cuda()
+            T = T.cuda()
+
+        batch_size = x.shape[0]
+        max_time = x.shape[1]
 
         # Convert raw params to normalized log-probs
         # Initial probabilities of starting in each state
-        log_pi = func.log_softmax(self.log_pi, dim=0)  # shape: (M,)
-        # Transition Prob Matrix
-        log_A = func.log_softmax(self.log_A, dim=1)  # shape: (M, M)
-        # Emission Prob matrix
-        log_B = func.log_softmax(self.log_B, dim=1)  # shape: (M, V)
+        log_pi = func.log_softmax(self.pi, dim=0)  # shape: (M,)
+        log_alpha = torch.zeros(batch_size, max_time, self.num_states)
+        if self.is_cuda:
+            log_alpha = log_alpha.cuda()
 
-        # alpha[t, i] = log P(o_1, ..., o_t, z_t = i)
         # Initialize alpha[0]
         # Represents the prob of being in each state at time 0 given the first observation
         # Adding all prior probs with the emission probs for the first observation.
         # Notice how what would have been multiplying probs turns into adding due to log-space
-        alpha = (
-            log_pi + log_B[:, obs_seq[0]]
-        )  # shape: (M,) where m is the number of states
+        log_alpha[:, 0, :] = self.emission_model[x[:, 0]] + log_pi
 
         # Recursively compute alpha for t=1..T-1
         # Alpha is a tensor representing the log of probs up to time t-1 for each state
@@ -90,14 +117,18 @@ class DiscreteHMM(nn.Module):
             # alpha_next[i] = logsumexp over j of [alpha[j] + A[j, i]] + B[i, obs[t]]
             # This combines transition and emission probabiilities to compute the state probabilties at each time step
             # I do this by summing over columns (previous states) for each row (current state)
-            alpha_next = (
-                torch.logsumexp(alpha.unsqueeze(1) + log_A, dim=0)
-                + log_B[:, obs_seq[t]]
+            log_alpha[:, t, :] = (
+                self.emission_model[x[:, t]]
+                + self.transition_model[log_alpha[:, t - 1, :]]
             )
 
-            alpha = alpha_next
-
         # P(obs_seq) = logsumexp(alpha[T-1, :])
-        # Finding log of the probability of an observation sequence.
-        log_prob = torch.logsumexp(alpha, dim=0)
-        return log_prob
+        # Finding log of the probability of an observation sequence by looking summing at the final timestamp
+        log_sums = log_alpha.logsumexp(dim=2)
+        log_probs = torch.gather(log_sums, 1, T.view(-1, 1) - 1)
+        return log_probs
+
+    # def emission_model_forward(self, x_t):
+    #     log_emission_matrix = func.log_softmax(self.emission_matrix, dim=1)
+    #     out = log_emission_matrix[:, x_t].transpose(0, 1)
+    #     return out
